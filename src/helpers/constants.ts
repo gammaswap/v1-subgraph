ed201c41a58939453b541401625881652cda2fb0
Kalesberg <mobaddicts@hotmail.com>
2023-09-05 15:27:24 -0400

    redeployed contracts

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,8 +4,8 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const MAX_UINT = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
-export const POSITION_MANAGER = '0xe5932d00d16612e9c5ccf16f8591e559b127aa94';
+export const POSITION_MANAGER = '0x8b0a5abf6ed4400feb29ab111351f9134ff38fd3';
 
-export const WETH_USDC_POOL = '0x79163cab23555b749ab8c84e4a987ebee625ffaf';
+export const WETH_USDC_POOL = '0x54b75d7614b1233e965abc2bd4823ac7607f7a34';
 
-export const POOL_VIEWER = '0x20a3374e5f26fdaa2f27652ddf89b59f19810923';
+export const POOL_VIEWER = '0xe0a2fc73008126d5451018c4f8a39929edd3eaa5';
