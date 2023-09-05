2876be56f1d93aa794008cf3b120f70088f02092
Kalesberg <mobaddicts@hotmail.com>
2023-09-04 21:15:16 -0400

    use latest contracts on arb goerli

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,11 +1,11 @@
+/// !!IMPORTANT!!: Make all addresses lowercase to avoid any weird behaviors
+
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const MAX_UINT = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
-export const POSITION_MANAGER = '0xaeffb59c9b2f571f7c50c4db64ff627966f2041d';
+export const POSITION_MANAGER = '0xe5932d00d16612e9c5ccf16f8591e559b127aa94';
 
-export const WETH_USDC_POOL = '0x8ae545f59b8119656cc9a90ec46c26b3cc35c9f9';
+export const WETH_USDC_POOL = '0x79163cab23555b749ab8c84e4a987ebee625ffaf';
 
-export const UNIV2_FACTORY = '0x6f44ff7d078a59404de2f446f95b8638369ac7e8';
-
-export const POOL_VIEWER = '0x03298b98b871c0ee3d5a31abec9cbd8326789d03';
+export const POOL_VIEWER = '0x20a3374e5f26fdaa2f27652ddf89b59f19810923';
