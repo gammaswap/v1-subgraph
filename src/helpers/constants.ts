e31f3cbacaa4116920464b7bf50540df03d0a9dc
Kalesberg <mobaddicts@hotmail.com>
2023-09-17 10:17:57 -0400

    new abis, new contracts

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,8 +4,8 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const MAX_UINT = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
-export const POSITION_MANAGER = '0xbbd32dd63484ae82f5539568e5b9ea6e50a57a4f';
+export const POSITION_MANAGER = '0xe94f83a0dbb5374df098ac64393fc5a5427dbc48';
 
-export const WETH_USDC_POOL = '0x0d5119c66806875d93c6c06d79f94eba6707e433';
+export const WETH_USDC_POOL = '0xdaeaffdbadb20b86f1863aa80355afc886c95761';
 
-export const POOL_VIEWER = '0x3871014c9606c3cd7a5c5a3a06a2edd74169f600';
+export const POOL_VIEWER = '0x268085e7c9a4a92ff7e4a73d33d21e3827c86192';
