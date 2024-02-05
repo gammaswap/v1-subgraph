58889b559d701a035ce67371d0f75b3455225158
Kalesberg <mobaddicts@hotmail.com>
2024-02-05 10:24:05 -0500

    typo

--- src/mappings/dsFactory.ts
+++ src/mappings/dsFactory.ts
@@ -2,7 +2,7 @@ import { DeltaSwapPair as DeltaSwapPairSource } from '../types/templates';
 import { PairCreated } from '../types/DeltaswapFactory/DeltaSwapFactory';
 import { createPair } from '../helpers/loader';
 
-export function handlePairCreate(event: PairCreated): void {
+export function handlePairCreated(event: PairCreated): void {
   createPair(event);
 
   DeltaSwapPairSource.create(event.params.pair);
