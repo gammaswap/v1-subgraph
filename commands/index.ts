5ad2c3b781181024ca87c054619ec0d28bb41bd8
Kalesberg <mobaddicts@hotmail.com>
2024-02-07 21:57:35 -0500

    remove unused code

--- commands/index.ts
+++ commands/index.ts
@@ -38,18 +38,6 @@ program
   .argument('<network>')
   .action(async (graph_uri: string, network: string) => 
   {
-    /**
-      "deploy:arbsepolia": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbsepolia subgraph.arbitrum-sepolia.yaml",
-      "deploy:arbsepolia-dev": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbsepolia-dev subgraph.arbitrum-sepolia.yaml",
-      "deploy:arbsepolia-oct23": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbsepolia-oct23 subgraph.arbitrum-sepolia-oct23.yaml",
-      "deploy:arbitrum": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbitrum subgraph.arbitrum.yaml",
-      "deploy:arbitrum-dev": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbitrum-dev subgraph.arbitrum.yaml",
-      "deploy:arbitrum-oct23": "graph deploy --product hosted-service gammaswap/gammaswap-v1-arbitrum-oct23 subgraph.arbitrum-oct23.yaml",
-      "deploy:sepolia": "graph deploy --product hosted-service gammaswap/gammaswap-v1-sepolia subgraph.sepolia.yaml",
-      "deploy:sepolia-dev": "graph deploy --product hosted-service gammaswap/gammaswap-v1-sepolia-dev subgraph.sepolia.yaml",
-      "deploy:dev": "graph deploy --product hosted-service gammaswap/gammaswap-dev subgraph.arbitrum-sepolia.yaml"
-     */
-
     exec(`graph deploy --product hosted-service ${graph_uri} subgraph.${network}.yaml`);
   })
 
